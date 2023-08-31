d6ea9e8a233c46ff77cabb1bea84678bafaf8fc8
lait <lait.zhang@gmail.com>
2023-08-31 14:04:47 +0800

    fix39 mysql init

--- src/main/java/org/dows/cloud/App.java
+++ src/main/java/org/dows/cloud/App.java
@@ -1,48 +1,48 @@
-package org.dows.cloud;
-
-import cn.hutool.json.JSONUtil;
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-import lombok.extern.slf4j.Slf4j;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * Hello world!ddddd
- */
-@Slf4j
-@EnableConfigurationProperties(AppVersion.class)
-@RestController
-@SpringBootApplication(scanBasePackages = {"org.dows.*"})
-@EnableEncryptableProperties
-public class App {
-    @Autowired
-    private AppVersion appVersion;
-
-    @Autowired
-    private DataSourceProperties dataSourceProperties;
-    @Value("${spring.profiles.active}")
-    private String env;
-
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
-
-    @GetMapping("/version")
-    public String version() {
-        log.info("version......");
-        appVersion.setEnv(env);
-        return JSONUtil.toJsonStr(appVersion);
-    }
-
-    @GetMapping("/env")
-    public String getDataSourceProperties() {
-        return JSONUtil.toJsonStr(dataSourceProperties);
-    }
-}
+//package org.dows.cloud;
+//
+//import cn.hutool.json.JSONUtil;
+//import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+//import lombok.extern.slf4j.Slf4j;
+//import org.mybatis.spring.annotation.MapperScan;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * Hello world!ddddd
+// */
+//@Slf4j
+//@EnableConfigurationProperties(AppVersion.class)
+//@RestController
+//@SpringBootApplication(scanBasePackages = {"org.dows.*"})
+//@EnableEncryptableProperties
+//public class App {
+//    @Autowired
+//    private AppVersion appVersion;
+//
+//    @Autowired
+//    private DataSourceProperties dataSourceProperties;
+//    @Value("${spring.profiles.active}")
+//    private String env;
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(App.class, args);
+//    }
+//
+//    @GetMapping("/version")
+//    public String version() {
+//        log.info("version......");
+//        appVersion.setEnv(env);
+//        return JSONUtil.toJsonStr(appVersion);
+//    }
+//
+//    @GetMapping("/env")
+//    public String getDataSourceProperties() {
+//        return JSONUtil.toJsonStr(dataSourceProperties);
+//    }
+//}
--- src/main/java/org/dows/cloud/AppVersion.java
+++ src/main/java/org/dows/cloud/AppVersion.java
@@ -1,19 +1,19 @@
-package org.dows.cloud;
-
-
-import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-import java.util.List;
-
-@ConfigurationProperties("dows.dxz")
-@Data
-public class AppVersion {
-    private String appId;
-    private String appCode;
-    private String version;
-    private List<String> modules;
-    private String env;
-
-
-}
+//package org.dows.cloud;
+//
+//
+//import lombok.Data;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//
+//import java.util.List;
+//
+//@ConfigurationProperties("dows.dxz")
+//@Data
+//public class AppVersion {
+//    private String appId;
+//    private String appCode;
+//    private String version;
+//    private List<String> modules;
+//    private String env;
+//
+//
+//}
